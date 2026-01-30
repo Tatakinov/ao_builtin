c2b2c543729954c45e13ff1601e5c7e93c8da56c
Tatakinov <tatakinov@gmail.com>
2026-01-30 22:46:35 +0900

    必要なくなった処理/ファイルを削除した。

--- util.h
+++ util.h
@@ -34,8 +34,6 @@ namespace util {
     SDL_DisplayID getNearestDisplay(int x, int y);
 
     std::string readDescript(std::filesystem::path path);
-
-    SDL_DisplayID getCurrentDisplayID();
 }
 
 #endif // UTIL_H_
